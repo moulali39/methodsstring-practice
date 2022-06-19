ab040ad6b33afe9aea67222777379af478331b3c
moulali39 <107177595+moulali39@users.noreply.github.com>
2022-06-19 16:02:54 +0530

    Add files via upload

---
+++ strings_methods (1).py
@@ -0,0 +1,59 @@
+a='moulali'
+print(a.capitalize())
+
+b='kodavatikallu'
+print(b.capitalize())
+
+c='hyderabad'
+print(c.capitalize())
+
+d='jaffer khan'
+print(d.capitalize()) 
+
+#title method where to show as smaller case into upper case letter
+a='computer programming'
+print(a.title())
+
+b='ratna hyrots'
+print(b.title())
+
+a='hello world'
+print(a.title())
+
+#istitle()
+d= 'hammmer'
+print(d.istitle())
+
+a='Andrapradesh'
+print(a.istitle())
+
+#upper()
+# a='moulali'
+# print(a.upper())
+
+# b= 'python practice'
+# print(b.upper())
+
+# c='shaik moulali'
+# print(c.upper())
+
+# lower()
+# a='ANDRAPREDESH'
+# print(a.lower())
+
+#isupper
+# a='aNDRAPREDESH'
+# print(a.isupper())
+
+#islower
+# a='telugu computer world'
+# print(a.islower())
+
+a='telugu computer world'
+print(a.find('c')) #find the charecter no in string  
+# print(a.find('o'))
+# print(a.index('u',4,6))#find the charecter no in string 
+
+# a='josh innovations'
+# print(len(a));c='hyderabad'# length to find the length of the string
+# print(len(c))
